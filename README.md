be22f6b2d8019327c65e077e9c29097d1c0cb679
Lu <luwicked2069@gmail.com>
2020-12-07 17:26:59 +0100

    - Add button fixed - Create a new loop function for the class animation (left, right, center) - Change the width of the mobile phone - Remove property - Add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1 align="center">Web Template Desktop 👇</h1>
+<p align="center"></p>
+<p align="center"><img src="3_booker.jpg"/></p> 
+
+## Autores ✒️
+
+* **Luis Mendoza** - *Trabajo Inicial* - [wikiesp](https://github.com/wikiesp)
--- app.js
+++ app.js
@@ -1,6 +1,30 @@
+// button responsive (hamburger)
+
 const burger = document.getElementById('hamburger');
 const liright = document.getElementById('nav_hamburger');
 
 burger.addEventListener('click', () => {
-  liright.classList.toggle('show');
+	liright.classList.toggle('show');
 });
+
+//Get the button
+var mybutton = document.getElementById('btop');
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = 'block';
+	} else {
+		mybutton.style.display = 'none';
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
